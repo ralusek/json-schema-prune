0576881a52fd3fe38274c8ca8c8a57f68c36e51f
ralusek <ralusek@gmail.com>
2018-09-06 20:36:09 -0700

    initialized.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function formatData(schema, data) {
+  if (data === undefined || data === null) return data;
+
+  if (schema.type === 'array') return data.map(item => formatData(schema.items, item));
+  else if (schema.type === 'object') {
+    return Object.keys(schema.properties).reduce((formatted, key) => {
+      const value = formatData(schema.properties[key], data[key]);
+      if (value !== undefined) formatted[key] = value;
+      return formatted;
+    }, {});
+  }
+  return data;
+}
+
+module.exports = formatData;
