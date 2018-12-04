7b621fcee59fbb1ee527ff67523ea4d6de4d98d0
ralusek <ralusek@gmail.com>
2018-12-04 10:59:55 -0800

    Changed so that empty arrays or objects are not included, after accounting for the addition of default values.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@
 
 function formatData(schema, data) {
   if (!schema) return data;
-  if (schema.type === 'array') return (data || []).map(item => formatData(schema.items, item));
+  if (schema.type === 'array') {
+  	const children = (data || []).map(item => formatData(schema.items, item));
+    return children.length ? children : undefined;
+  }
   else if (schema.type === 'object') {
     data = data || {};
-    return Object.keys(schema.properties).reduce((formatted, key) => {
-      if (!data[key]) return formatted;
+    const child = Object.keys(schema.properties).reduce((formatted, key) => {
       const value = formatData(schema.properties[key], data[key]);
       if (value !== undefined) formatted[key] = value;
       return formatted;
     }, {});
+    return Object.keys(child).length ? child : undefined;
   }
 
   return data === undefined ? schema.default : data;
