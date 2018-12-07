abe40adf4c6ee193b7aacf3a7ff026238f086a61
ralusek <ralusek@gmail.com>
2018-12-06 16:40:13 -0800

    Account for object.properties not being defined.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
 function formatData(schema, data) {
   if (!schema) return data;
   if (schema.type === 'array') {
-  	const children = (data || []).map(item => formatData(schema.items, item));
+    const children = (data || []).map(item => formatData(schema.items, item));
     return children.length ? children : undefined;
   }
   else if (schema.type === 'object') {
     data = data || {};
-    const child = Object.keys(schema.properties).reduce((formatted, key) => {
+    const child = Object.keys(schema.properties || {}).reduce((formatted, key) => {
       const value = formatData(schema.properties[key], data[key]);
       if (value !== undefined) formatted[key] = value;
       return formatted;
